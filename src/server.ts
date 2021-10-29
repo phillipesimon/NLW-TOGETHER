56b01ec9934fb753fe607d79137818f84b67367a
phillipesimon <phillipe.simon@gmail.com>
2021-10-29 09:31:27 -0300

    Aula 03 - Tratamento de erros configurado

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 import { router } from './routes';
 
@@ -11,6 +12,22 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+// Middlewares para tratar os erros(deve conter 4 parametros) sempre depois das rotas
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error!',
+    });
+  },
+);
 
 app.listen(3333, function () {
   return console.log('Server is running on port 3333 🚀🚀🚀!');
