2ce5e424a57f11edc1a2cf79aebf643e473a00ef
phillipesimon <phillipe.simon@gmail.com>
2021-10-30 13:08:55 -0300

    Fim Aula 04 - Estrurura compliments ok

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
 
 import { ensureAdmin } from './middlewares/ensureAdmin';
 
@@ -12,10 +13,12 @@ const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 // Definindo as rotas
 router.post('/users', ensureAdmin, createUserController.handle);
 router.post('/tags', createTagController.handle);
 router.post('/login', authenticateUserController.handle);
+router.post('/compliment', createComplimentController.handle);
 
 export { router };
